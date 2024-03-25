af29d528a26b808ca1369def8dbe2454d9474a84
Ryan Fowler <ryan.fowler19@gmail.com>
2024-03-24 21:56:18 -0700

    nvim: enable gofumpt and staticcheck

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 -- neovim configuration
 
 -- Set <space> as the leader key
@@ -468,7 +469,14 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				gopls = {},
+				gopls = {
+					settings = {
+						gopls = {
+							gofumpt = true,
+							staticcheck = true,
+						},
+					},
+				},
 				-- pyright = {},
 				rust_analyzer = {},
 				zls = {},
