4476c706e366c353e397e9fa0056fd67c3c32831
Ryan Fowler <ryan.fowler19@gmail.com>
2024-04-11 18:56:46 -0700

    nvim: use mini for the lsp message view

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -737,7 +737,9 @@ require("lazy").setup({
 				},
 			},
 			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				message = {
+					view = "mini",
+				},
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
