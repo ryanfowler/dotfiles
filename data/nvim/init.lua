4e70341fc6d172c80f36b95051fed47716ca5a02
Ryan Fowler <ryan.fowler19@gmail.com>
2024-04-14 18:49:27 -0700

    nvim: show leading spaces

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -47,7 +47,7 @@ vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "» ", lead = "·", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
