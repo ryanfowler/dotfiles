4ae39d6213f4a7c6e165d43851ce6b413f9c018d
Ryan Fowler <ryan.fowler19@gmail.com>
2024-05-02 21:46:28 -0700

    nvim: disable hardtime by default

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -806,7 +806,9 @@ require("lazy").setup({
 	{
 		"m4xshen/hardtime.nvim",
 		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
+		opts = {
+			enabled = false,
+		},
 	},
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
