e8343dd961d5c84adc2d5f3eedd730e0194232e8
Ryan Fowler <ryan.fowler19@gmail.com>
2024-09-30 08:37:46 -0700

    nvim: enable enter for autocomplete

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -648,6 +648,9 @@ require("lazy").setup({
 					--  This will auto-import if your LSP supports it.
 					--  This will expand snippets if the LSP sent a snippet.
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
