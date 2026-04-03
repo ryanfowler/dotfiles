609dc69e2d39b27dce12c476a6f237d3abc70158
Ryan Fowler <ryan.fowler19@gmail.com>
2026-04-02 21:28:58 -0700

    nvim: add JavaScript and TypeScript support
    Add treesitter parsers (javascript, typescript, tsx) for syntax
    highlighting and ts_ls language server for LSP support.

--- data/nvim/lua/plugins/treesitter.lua
+++ data/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "c", "go", "html", "lua", "luadoc", "markdown", "rust" },
+		ensure_installed = { "bash", "c", "go", "html", "javascript", "lua", "luadoc", "markdown", "rust", "typescript", "tsx" },
 		auto_install = true,
 		highlight = {
 			enable = true,
