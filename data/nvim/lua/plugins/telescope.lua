1122635b58a06860507c9e416d7dc13f584c6d33
Ryan Fowler <ryan.fowler19@gmail.com>
2026-04-08 23:15:56 -0700

    Add Telescope picker for changed git files with preview

--- data/nvim/lua/plugins/telescope.lua
+++ data/nvim/lua/plugins/telescope.lua
@@ -62,7 +62,76 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		-- See `:help telescope.builtin`
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
 		local builtin = require("telescope.builtin")
+		local config = require("telescope.config").values
+		local finders = require("telescope.finders")
+		local pickers = require("telescope.pickers")
+
+		local function git_changed_files()
+			local git_root = vim.fn.systemlist({ "git", "rev-parse", "--show-toplevel" })[1]
+			if vim.v.shell_error ~= 0 or not git_root or git_root == "" then
+				vim.notify("Not inside a git repository", vim.log.levels.WARN)
+				return
+			end
+
+			local output = vim.fn.systemlist({ "git", "-C", git_root, "status", "--short", "--untracked-files=all" })
+			if vim.v.shell_error ~= 0 then
+				vim.notify("Failed to read git status", vim.log.levels.ERROR)
+				return
+			end
+
+			local items = {}
+			for _, line in ipairs(output) do
+				local status = line:sub(1, 2)
+				local path = line:sub(4)
+
+				if status ~= " D" and status ~= "D " and path ~= "" then
+					path = path:gsub(" -> ", "\t")
+					local current_path = path:match("\t(.+)$") or path
+					table.insert(items, {
+						status = status,
+						path = current_path,
+						display = string.format("%s %s", status, current_path),
+					})
+				end
+			end
+
+			if vim.tbl_isempty(items) then
+				vim.notify("No modified or untracked files", vim.log.levels.INFO)
+				return
+			end
+
+			pickers.new({}, {
+				prompt_title = "Git Changed Files",
+				cwd = git_root,
+				finder = finders.new_table({
+					results = items,
+					entry_maker = function(entry)
+						local full_path = git_root .. "/" .. entry.path
+						return {
+							value = entry.path,
+							display = entry.display,
+							ordinal = entry.display,
+							filename = full_path,
+							path = entry.path,
+						}
+					end,
+				}),
+				previewer = config.file_previewer({}),
+				sorter = config.generic_sorter({}),
+				attach_mappings = function(prompt_bufnr)
+					actions.select_default:replace(function()
+						local selection = action_state.get_selected_entry()
+						actions.close(prompt_bufnr)
+						vim.cmd.edit(vim.fn.fnameescape(git_root .. "/" .. selection.path))
+					end)
+					return true
+				end,
+			}):find()
+		end
+
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
@@ -76,6 +145,7 @@ return {
 		vim.keymap.set("n", "<leader>sj", builtin.jumplist, { desc = "[S]earch [J]umplist" })
 		vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "[G]it [C]ommits" })
 		vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "[G]it [S]tatus" })
+		vim.keymap.set("n", "<leader>gm", git_changed_files, { desc = "[G]it [M]odified files" })
 
 		local extensions = require("telescope").extensions
 		vim.keymap.set("n", "<leader>fb", function()
